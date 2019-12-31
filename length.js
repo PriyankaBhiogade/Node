e3683ddd8338dc9388cf9dcbb320f15858ac3ae1
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-12-31 12:13:22 +0530

    Refactor : code rename enum name

--- length.js
+++ length.js
@@ -1,25 +1,24 @@
-var Enum = require('enum');
-const myEnum = new Enum({
+let Enum = require('enum');
+const lengthEnum = new Enum({
     FEET: 12.0,
     INCH: 1.0,
     YARD: 36.0,
     CENTIMETER: 0.4
 });
 class Length {
-    
     conversion(unit) {
-        if (unit == myEnum.INCH) {
-            return myEnum.getValue(`INCH`);
-        } else if (unit == myEnum.FEET) {
-            return myEnum.getValue(`FEET`);
-        } else if (unit == myEnum.YARD) {
-            return myEnum.getValue(`YARD`);
-        } else if (unit == myEnum.CENTIMETER) {
-            return myEnum.getValue(`CENTIMETER`);
+        if (unit == lengthEnum.INCH) {
+            return lengthEnum.getValue(`INCH`);
+        } else if (unit == lengthEnum.FEET) {
+            return lengthEnum.getValue(`FEET`);
+        } else if (unit == lengthEnum.YARD) {
+            return lengthEnum.getValue(`YARD`);
+        } else if (unit == lengthEnum.CENTIMETER) {
+            return lengthEnum.getValue(`CENTIMETER`);
         }
-    }                           
+    }
 }
 module.exports = {
-    length: new Length,
-    enum: Object.freeze(myEnum)
+    length: Length,
+    lengthEnum: Object.freeze(lengthEnum)
 };
